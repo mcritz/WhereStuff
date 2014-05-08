2ce491e8d66c03e621292612e27585bab446e0ed
Michael Critz <github@michaelcritz.com>
2014-05-08 14:20:21 -0700

    Map Annotation Model

---
+++ Where Car?/CAR_MapAnnotation.h
@@ -0,0 +1,20 @@
+//
+//  CAR_MapAnnotation.h
+//  Where Car?
+//
+//  Created by Michael Critz on 5/8/14.
+//  Copyright (c) 2014 Map of the Unexplored. All rights reserved.
+//
+
+#import <Foundation/Foundation.h>
+#import <MapKit/MapKit.h>
+
+@interface CAR_MapAnnotation : NSObject <MKAnnotation>
+
+@property (nonatomic, copy)NSString *title;
+@property (nonatomic, copy)NSString *subTitle;
+@property (nonatomic)CLLocationCoordinate2D coordinate;
+
+- (void)setCoordinate:(CLLocationCoordinate2D)newCoordinate;
+
+@end
---
+++ Where Car?/CAR_MapAnnotation.m
@@ -0,0 +1,36 @@
+//
+//  CAR_MapAnnotation.m
+//  Where Car?
+//
+//  Created by Michael Critz on 5/8/14.
+//  Copyright (c) 2014 Map of the Unexplored. All rights reserved.
+//
+
+#import "CAR_MapAnnotation.h"
+
+@interface CAR_MapAnnotation()
+
+@property (nonatomic)MKAnnotationView *annotationView;
+
+@end
+
+@implementation CAR_MapAnnotation
+
+@synthesize subtitle, title, coordinate;
+
+- (MKAnnotationView *)annotationView {
+	if (!_annotationView) {
+		CGRect newFrame = CGRectMake(0., 0., 10., 10.);
+		_annotationView = [[MKAnnotationView alloc] initWithFrame:newFrame];
+		[_annotationView setBackgroundColor:[UIColor colorWithRed:230. green:115. blue:255 alpha:.7]];
+	}
+	return _annotationView;
+}
+
+- (void)setCoordinate:(CLLocationCoordinate2D)newCoordinate {
+	if (CLLocationCoordinate2DIsValid(newCoordinate)) {
+		coordinate = newCoordinate;
+	}
+}
+
+@end
